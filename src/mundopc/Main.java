d5584c786ab187f739544b1a7b416dba69c9f2d6
William Lopez R <lopezrebellonw@gmail.com>
2024-11-27 22:23:52 -0500

    creación de clase monitor

--- src/mundopc/Main.java
+++ src/mundopc/Main.java
@@ -1,9 +1,19 @@
 package mundopc;
 
+import mundopc.modelo.Monitor;
+import mundopc.modelo.Raton;
+import mundopc.modelo.Teclado;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // crear objetos
+        Raton ratonLenovo = new Raton("bluetooh", "Lenovo");
+        System.out.println(ratonLenovo);
+        Teclado tecladoLenovo = new Teclado("bluetooh", "lenovo");
+        System.out.println(tecladoLenovo);
+        Monitor monitorLenovo = new Monitor("Lenovo", 27);
+        System.out.println(monitorLenovo);
     }
 }
